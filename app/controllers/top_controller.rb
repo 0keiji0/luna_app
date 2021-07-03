bd1f02ba5e0bd94523a1bc712da86b0b5aed176f
0keiji0 <keiji.goto213@gmail.com>
2021-07-03 13:46:29 +0000

    静的ページ作成

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,17 @@
+class TopController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def login
+  end
+  
+  def privacypolicy
+  end
+  
+  def company
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/' => 'top#home'
+  get '/login' => 'top#login'
+  get '/help' => 'top#help'
+  get '/company' => 'top#company'
+  get '/privacy' => 'top#privacy'
+  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

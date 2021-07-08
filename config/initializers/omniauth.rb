ba0cb216c2bb7ba7fa1484136e69c1b61e6c3a1e
0keiji0 <keiji.goto213@gmail.com>
2021-07-08 13:58:00 +0000

    loginsession

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['8KjvJxFjdxTmoYC23rDMc6Ou8'], ENV['oV9di569wzGEG6bzn6a7JEGoth1PYgqFs6fGAIpdoQDsqdGEiC']
+    provider :twitter,Rails.gTJfwF04p1PdPviBMLwDTk83c,Rails.lYhb0kRtaKwqdqh7g4EL220JnCXM88lmugDzULuIMLFVnUrNaN
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/privacy' => 'top#privacy'
   get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
